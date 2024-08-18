02a4e1245fe007031f1e96531cea892c68f65185
alexander shen <alexander.shen@gmail.com>
2024-08-18 16:05:38 -0400

    modified navbar styling

--- app/global-components/Navbar.jsx
+++ app/global-components/Navbar.jsx
@@ -9,21 +9,21 @@ export function MainNav() {
   console.log(user)
 
   return (
-    <div className="bg-app-light-purple bb-2 p-4">
-      <Link href="/"className="animate-fade-in rounded-full px-4 py-1.5 text-sm font-medium text-gray-500 transition-colors ease-out hover:text-black dark:text-white dark:hover:text-white/70">
+    <div className="py-4 px-10">
+      <Link href="/"className="animate-fade-in rounded-full px-4 py-1.5 text-xl font-medium transition-colors ease-out text-app-black hover:text-black">
         LOGO HERE
       </Link>
       
-      <>
+      <div className="float-right">
         <Link href="/login"
-          className="animate-fade-in rounded-full px-4 py-1.5 text-sm font-medium text-gray-500 transition-colors ease-out hover:text-black dark:text-white dark:hover:text-white/70">
-          Login
+          className="animate-fade-in rounded-full px-4 py-1.5 text-md font-medium text-app-black transition-colors ease-out hover:text-black">
+          Log in
         </Link>
         <Link href="/register"
-          className="animate-fade-in rounded-full border border-black bg-black px-4 py-1.5 text-sm text-white transition-all hover:bg-gray-800 hover:ring-4 hover:ring-gray-200 dark:border-white dark:bg-white dark:text-gray-600 dark:hover:bg-white dark:hover:text-gray-800 dark:hover:hover:shadow-[0_0_25px_5px_rgba(256,256,256,0.2)] dark:hover:ring-0">
-          Signup
+          className="animate-fade-in rounded-full border border-black bg-black px-4 py-1.5 text-md text-white transition-all hover:bg-gray-800 hover:ring-4 hover:ring-app-dark-purple">
+          Sign up
         </Link>
-      </>
+      </div>
     </div>
   )
 }
