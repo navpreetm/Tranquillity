94973c3b664eb73fa1052763715e70b439f244b4
Navpreet Multani <navpreet@umich.edu>
2024-08-22 22:39:24 -0400

    Vercel Test

--- app/(private)/dashboard/page.jsx
+++ app/(private)/dashboard/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/firebaseApp";
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Dashboard() {
   const [user, loading, error] = useAuthState(auth);
-  console.log("fetching dashboard")
+  console.log("fetching dashboard");
 
   // need to perform a fetch for:
   // total streak data
@@ -15,24 +15,28 @@ export default function Dashboard() {
   // Gets the first name using the firebase display name
   const getFirstName = (name) => {
     return name.split(" ")[0];
-  }
+  };
 
   return (
     <div className="p-8">
-      <h1 className="text-4xl" > Hello {getFirstName(user.displayName)}!</h1>
-      <p className="text-xl">Your daily streak is <span className="text-app-purple-700">11</span></p>
+      <h1 className="text-4xl"> Hello {getFirstName(user.displayName)}!</h1>
+      <p className="text-xl">
+        Your daily streak is <span className="text-app-purple-700">11</span>
+      </p>
 
-      <h2 className="text-2xl mt-12">Today's Tasks</h2>
+      <h2 className="text-2xl mt-12">Today&apos;s Tasks</h2>
       <ul className="list-none mt-4">
         <li className="text-xl">
-          <i className="fas fa-square"></i> write a <Link 
-          href="/journal-entries"
-          className="text-purple-600 underline">journal entry</Link>
+          <i className="fas fa-square"></i> write a{" "}
+          <Link href="/journal-entries" className="text-purple-600 underline">
+            journal entry
+          </Link>
         </li>
         <li className="text-xl mt-2">
-          <i className="fas fa-square"></i> complete <Link 
-          href="/mood-tracker"
-          className="text-purple-600 underline">mood tracker</Link>
+          <i className="fas fa-square"></i> complete{" "}
+          <Link href="/mood-tracker" className="text-purple-600 underline">
+            mood tracker
+          </Link>
         </li>
       </ul>
     </div>
